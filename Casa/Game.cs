0425fa2288b98626ae6b02c466ee3c4896b217b9
Mauricio Sauza <mauriciosauzatorrez666@gmail.com>
2021-10-15 20:02:47 -0400

    First commit

---
+++ Casa/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Casa
+{
+    class Game 
+    {
+        GameWindow window;
+        public Game(GameWindow window)
+        {
+            this.window = window;
+            Start();
+        }
+        public void Start()
+        {
+            window.Load += loaded;
+            window.Resize += resize;
+            window.RenderFrame += renderF;
+            window.Run();
+        }
+        public void resize(object o, EventArgs e)
+        {
+            GL.Viewport(0, 0, window.Width, window.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+        public void renderF(object o, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex2(1.0, 1.0);
+            GL.Vertex2(49.0, 1.0);
+            GL.Vertex2(25.0, 49.0);
+            GL.End();
+            window.SwapBuffers();
+        }
+        public void loaded(object o, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ Casa/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace Casa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow(800, 600);
+            Game gm = new Game(window);
+        }
+    }
+}
